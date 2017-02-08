65cb2fada9f24e31bb1cfd92aa039f3ce32067dc
“antraxalan <aruquipa.alan@gmail.com>
2017-02-08 08:08:23 -0400

    sea_changes

---
+++ contactform/contactform.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.book_now_fish').on('click',function (event) {
+    window.location.href = "https://squareup.com/appointments/book/DMZGPSYGTSFBN/sea-monkey-charters-llc";
+    // window.location.replace("https://squareup.com/appointments/book/DMZGPSYGTSFBN/sea-monkey-charters-llc");
+  });
+  $('.go_to_booking').on('click',function (event) {
+    window.location.href = "https://squareup.com/appointments/book/DMZGPSYGTSFBN/sea-monkey-charters-llc";
+  });
+
+});
